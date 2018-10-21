d3136533f2a3542e5ad27f04ef929e16ebedb41b
Cookie150cc <cooke9619@gmail.com>
2018-10-20 22:38:10 -0400

    makefile added

--- Source/demoQuadSolver.c
+++ Source/demoQuadSolver.c
@@ -1,6 +1,20 @@
+//Imported libraries
 #include <stdio.h>
 
+//Global define variables
+#define input_length 100
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  //Variable declaration/ Memory allocation
+  char input[input_length];
+  //Breif instructions
+  printf("Welcome to Quad Solver!\n\t plug in values for a, b, and c in that order\n\tSeperate the three values by a comma or a space\n");
+  //prompt
+  printf("\nEnter values >> ");
+  //get user input
+  scanf("%s", input);
+  printf("%s", input);
+
+
   return 0;
 }
